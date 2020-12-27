2c8a713f0b9c60094dd5898fb037cddf968bd042
mas98ahmed <ahmedmas@post.bgu.ac.il>
2020-12-27 19:34:43 +0200

    done the udp connection.

--- Client/main.py
+++ Client/main.py
@@ -1,4 +1,26 @@
-import socket
+from socket import *
+from struct import *
 
-udp_port_dest = 13117
-clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+def ConnectServer():
+    client_socket = socket(AF_INET,SOCK_DGRAM)
+    data, address = client_socket.recvfrom(1024)
+    msg = unpack(data)
+    if len(msg) == 3:
+        test_1 = msg[0]
+        test_2 = msg[1]
+        tcp_port = msg[3]
+    while len(msg) > 3 or test_1 is not 0xfeedbeef or test_2 is not 0x2:
+        data, address = client_socket.recvfrom(1024)
+        msg = unpack(data)
+        if len(msg) == 3:
+            test_1 = msg[0]
+            test_2 = msg[1]
+            tcp_port = msg[2]
+    return tuple(tcp_port, address)
+    
+
+if __name__ == "__main__":
+    print("Client started, listening for offer requests...")
+    tcp_port, address = ConnectServer()
+    print("Received offer from %s, attempting to connect...".format(address))
